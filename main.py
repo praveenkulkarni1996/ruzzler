84a8869f09db8dc76613c183477d37166233c52e
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2019-06-15 06:43:42 +0530

    sort in decreasing order of score
    currently ignore double/triple letter words

--- main.py
+++ main.py
@@ -6,5 +6,12 @@ def build_trie_from_filename(filename):
     add_words_from_file(root, filename)
     return root
 
-root = build_trie_from_filename('dictionary.txt')
-print sorted(list(set(Board(raw_input()).find_all_words(root))), key=len)
+root = build_trie_from_filename('dictionary.txt') #TODO(pickle this)
+
+# score of alphabets from A-Z
+score = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+def calc(word):
+    return sum([score[ord(char) - ord('a')] for char in word])
+
+print(sorted(list(set(Board(raw_input()).find_all_words(root))), key=calc, reverse=True))
